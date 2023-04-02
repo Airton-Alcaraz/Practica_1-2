3e11209540d4b57818b2c2df0f024a049b5f3038
Airton Alcaraz <airton.alcaraz94@gmail.com>
2023-04-02 17:26:16 -0300

    Practica 1-2

---
+++ algoritmo.js
@@ -0,0 +1,26 @@
+document.querySelector("body").innerHTML = `
+<iframe width="560" height="315" src="https://www.youtube.com/embed/51j-OHF3WEw" title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+
+    <p>Video random de youtube</p>
+
+
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.0167132768424!2d-58.38375908477038!3d-34.60373888045957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4aa9f0a6da5edb%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses!2sar!4v1680464251454!5m2!1ses!2sar"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    <p>Ubicacion del obelisco donde la gente siempre fija como un punto de encuentro</p>
+
+    <iframe style="border-radius:12px"
+        src="https://open.spotify.com/embed/track/164QjgWOxnSZYHQghRKeC0?utm_source=generator" width="100%" height="152"
+        frameBorder="0" allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    
+    <p>Cancion que no puedo dejar de escuchar</p>   
+    
+    <iframe height="900" width="1200" src="https://mangaplus.shueisha.co.jp/updates" frameborder="0"></iframe>
+    <p>Pagina para leer mangas</p>
+`
